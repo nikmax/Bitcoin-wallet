d06836bf06aad3a4389575e0039b99ae7480f95e
Anton Sander <mail@antonsander.de>
2026-07-06 11:29:13 +0200

    etwas aufgeräumt vor 4

--- termux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkg update
-pkg install python clang rust make openssl libffi
-python -m pip install --upgrade pip setuptools wheel
-
-pip install --no-cache-dir -r requirements.termux
-
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
-
