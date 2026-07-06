f5ad443883e0e6143390ca3fcb1be8b6b147c89d
Anton Sander <mail@antonsander.de>
2026-07-06 14:46:02 +0200

    kleinigkeiten qrcode pip

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /wallet
-pip3 install --no-cache-dir -r requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
 
 python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
